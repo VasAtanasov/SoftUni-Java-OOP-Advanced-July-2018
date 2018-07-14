747857baba56cc0c93249c2e476586de7b0e3204
Vasil Atanasov <vas.atanasov@gmail.com>
2018-07-14 18:33:46 +0300

    added tasks

--- src/L05IteratorsAndComparators/Ex02Library/Library.java
+++ src/L05IteratorsAndComparators/Ex02Library/Library.java
@@ -1,4 +1,30 @@
 package L05IteratorsAndComparators.Ex02Library;
 
-public class Library {
+import java.util.Iterator;
+
+public class Library<T> implements Iterable<T> {
+    private T[] ts;
+
+    public Library(T... ts) {
+        this.ts = ts;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LibraryIterator();
+    }
+
+    private final class LibraryIterator implements Iterator<T> {
+        private int count = 0;
+
+        @Override
+        public boolean hasNext() {
+            return this.count < ts.length;
+        }
+
+        @Override
+        public T next() {
+            return ts[count++];
+        }
+    }
 }
